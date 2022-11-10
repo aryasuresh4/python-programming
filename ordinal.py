411a48ae173781d619c2383ff21e378ce6b02dba
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2022-11-10 14:46:13 +0530

    Add files via upload

---
+++ ordinal.py
@@ -0,0 +1,4 @@
+a=input("enter a word")
+print([ord(i) for i in a])
+for i in a:
+           print("ordinal value of",i,"is :",ord(i))
---
+++ positive.py
@@ -0,0 +1,3 @@
+num=[15,25,-4,22,-32,-1,0,18]
+new_num=list(filter(lambda x:x>0,num))
+print("positive numbers in the list:",new_num)
